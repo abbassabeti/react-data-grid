bdb55d76c04aa88bdb3a528d2b3c35eb4290312a
Abbas Sabeti <as@bornak.de>
2020-02-15 14:40:20 +0330

    Trying to use both packages from my repo

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-export {ReactDataGrid} from './packages/react-data-grid/lib/index';
 export * from './packages/react-data-grid/lib/index';
 export * from  './packages/react-data-grid-addons/lib/index';

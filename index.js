d5e7ba77801cc3bca8b8fe39d9cc92f94a5f8d48
Abbas Sabeti <as@bornak.de>
2020-02-15 15:03:28 +0330

    Fixed: sth

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-export * from './packages/react-data-grid/lib/index';
+import * as ReactDataGrid from './packages/react-data-grid/lib/index';
 export * from  './packages/react-data-grid-addons/lib/index';
+export default ReactDataGrid;
